1b133e559e74648c171d34775ffed3137796d478
long88nguyen <longcdvt0403@gmail.com>
2022-05-23 17:30:38 +0700

    push master branch

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('locations.index');
+    return view('welcome');
 });
 
 Route::get('/location',[\App\Http\Controllers\Admin\LocationController::class,'index'])->name('location.index');
